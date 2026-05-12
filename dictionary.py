47450227535f33a7e877a19d47c2894b4cba2c38
Malik Ahsan <malikahsan13@gmail.com>
2026-05-12 22:55:09 +0500

    worked with dictionaries

---
+++ dictionary.py
@@ -0,0 +1,36 @@
+
+monthConversions = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December"
+}
+
+monthConversionsNumeric = {
+    0 : "January",
+    1 : "February",
+    2 : "March",
+    3 : "April",
+    4 : "May",
+    5 : "June",
+    6 : "July",
+    7 : "August",
+    8 : "September",
+    9 : "October",
+    10 : "November",
+    11 : "December"
+}
+
+print(monthConversions["Apr"])
+
+print(monthConversions.get("Dec"))
+
+print(monthConversions.get("abc" , "Not a valid key"))
