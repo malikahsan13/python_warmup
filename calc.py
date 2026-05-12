56ac8d2dc6d55a31011278fcb2343ee96a7462cf
Malik Ahsan <malikahsan13@gmail.com>
2026-05-12 22:38:53 +0500

    worked with conditions and comparison operators

--- calc.py
+++ calc.py
@@ -1,6 +1,15 @@
-num1 = input("Enter a number: ")
-num2 = input("enter another number: ")
+num1 = input("Enter num1 = ")
+num2 = input("Enter num2 = ")
+op = input("Enter operation +,-,/,* you want to perform")
 
-result = int(num1)+int(num2)
-
-print(result)
+if op == "+":
+    print("Sum is = ", int(num1) + int(num2))
+elif op == "-":
+    print("Subtraction is = ",int(num1) - int(num2))
+elif op == "/":
+    if int(num2) !=0:
+        print("Division num1/num2 = ", int(num1) / int(num2))
+    else:
+        print("Division not possible")
+elif op == "*":
+    print("Multiplication = ", int(num1) * int(num2))
