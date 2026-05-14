d5971e7d7e29ddc6aff158eac2c06e7351ba4595
Malik Ahsan <malikahsan13@gmail.com>
2026-05-14 23:45:24 +0500

    added uppercase and lower case handling for translation

--- translater.py
+++ translater.py
@@ -3,8 +3,11 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "g"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation + letter
     return translation
